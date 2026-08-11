ab872e87057c6bcfe1d4533f6dc8bb369f9e0caf
swapnil thite <thiteswapnil1212@gmail.com>
2026-08-11 21:59:37 +0530

    layout bug fixed

--- components/layout/AppShell.tsx
+++ components/layout/AppShell.tsx
@@ -11,10 +11,10 @@ export default function AppShell({
   children,
 }: AppShellProps) {
   return (
-    <div className="flex min-h-screen bg-[#09090B] text-white">
+    <div className="flex min-h-screen flex-col bg-[#09090B] text-white">
       <Navbar />
 
-      <main className="flex-1 overflow-hidden">
+      <main className="flex min-h-0 flex-1 overflow-hidden">
         {children}
       </main>
     </div>
