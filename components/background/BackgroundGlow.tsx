7cd4c5abc5bfb05fb4c7dfdd8d86b5b2e3d5ef12
swapnil thite <thiteswapnil1212@gmail.com>
2026-09-01 10:41:49 +0530

    removed fake glow

--- components/background/BackgroundGlow.tsx
+++ components/background/BackgroundGlow.tsx
@@ -1,17 +1,60 @@
-"use client";
+﻿"use client";
 
 export default function BackgroundGlow() {
   return (
-    <>
-      <div className="pointer-events-none fixed inset-0 -z-10 overflow-hidden">
+    <div
+      className="
+        pointer-events-none
+        fixed
+        inset-0
+        -z-10
+        overflow-hidden
+      "
+      aria-hidden="true"
+    >
+      {/* Subtle top ambient light */}
+      <div
+        className="
+          absolute
+          left-1/2
+          top-[-320px]
+          h-[520px]
+          w-[520px]
+          -translate-x-1/2
+          rounded-full
+          bg-[#D4AF37]/[0.045]
+          blur-[150px]
+        "
+      />
 
-        <div className="absolute left-1/2 top-[-250px] h-[600px] w-[600px] -translate-x-1/2 rounded-full bg-[#D4AF37]/10 blur-[180px]" />
+      {/* Very soft bottom light */}
+      <div
+        className="
+          absolute
+          bottom-[-300px]
+          right-[-220px]
+          h-[480px]
+          w-[480px]
+          rounded-full
+          bg-[#D4AF37]/[0.025]
+          blur-[160px]
+        "
+      />
 
-        <div className="absolute bottom-[-250px] right-[-150px] h-[500px] w-[500px] rounded-full bg-yellow-500/5 blur-[180px]" />
-
-        <div className="absolute left-[-150px] top-1/2 h-[450px] w-[450px] -translate-y-1/2 rounded-full bg-white/5 blur-[180px]" />
-
-      </div>
-    </>
+      {/* Soft neutral side light */}
+      <div
+        className="
+          absolute
+          left-[-220px]
+          top-1/2
+          h-[420px]
+          w-[420px]
+          -translate-y-1/2
+          rounded-full
+          bg-white/[0.018]
+          blur-[160px]
+        "
+      />
+    </div>
   );
 }
