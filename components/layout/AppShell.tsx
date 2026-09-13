d3d678bee2cca81b091c0454b3b5e6f5e44f4928
swapnil thite <thiteswapnil1212@gmail.com>
2026-09-13 19:11:47 +0530

    adds in main screen

--- components/layout/AppShell.tsx
+++ components/layout/AppShell.tsx
@@ -50,15 +50,14 @@ export default function AppShell({
 
       {/* Scrollable Content */}
       <main
-        className="
-          relative
-          z-10
-          min-h-0
-          flex-1
-          overflow-y-auto
-          overflow-x-hidden
-        "
-      >
+  className="
+    relative
+    z-10
+    min-h-0
+    flex-1
+    overflow-hidden
+  "
+>
         {children}
       </main>
     </div>
